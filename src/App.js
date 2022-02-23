07c2bdd9397fda9d4672321336b9d73dfa102a8a
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-24 00:30:20 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+    return React.createElement(
+      'div',
+     { className: 'container' }, 
+    React.createElement('h1', {}, 'My App')
+    )
+}
+
+export default App
