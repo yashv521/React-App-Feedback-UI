45615ef633055f0d073c2ed1fa69d47cecba332e
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-24 00:58:02 +0530

    jsx conditionals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react"
-
 function App() {
-    return React.createElement(
-      'div',
-     { className: 'container' }, 
-    React.createElement('h1', {}, 'My App')
+
+const title = 'Blog Post'
+const body = 'This is my blog post'
+const comments = [
+    {id: 1, text: 'Comment one'},
+    {id: 2, text: 'Comment two'},
+    {id: 3, text: 'Comment three'},
+]
+
+const loading = false
+const showComments = true
+
+if(loading) return <h1>Loading....</h1>
+
+    return (
+        <div className="container">
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            
+        {showComments && ( <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                       <li key={index}>comment.text</li> 
+                    ))}
+                </ul>
+            </div> 
+            )}
+        </div>
     )
 }
 
