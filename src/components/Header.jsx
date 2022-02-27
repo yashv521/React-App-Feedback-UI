4d4b55ebf20a34296d64972d92535e71780e69e9
yashv521 <55803892+yashv521@users.noreply.github.com>
2022-02-27 22:34:28 +0530

    first component and props

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+
+function Header({ text }) {
+  return (
+    <header>
+        <div className='container'>
+            <h2>
+                {text}
+            </h2>
+        </div>
+    </header>
+        
+
+  )
+}
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+}
+
+
+
+export default Header
